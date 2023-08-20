eaf65e40b7cc62c8f661dd5547d4bda8edca1b47
bobob0311 <qweasdzxc0311@naver.com>
2023-08-20 21:26:04 +0900

    Header

---
+++ src/Header.jsx
@@ -0,0 +1,28 @@
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <div>
+                    <a href="#">
+                        <div>
+                            Haniuem
+                        </div>
+                    </a>
+                    <ul>
+                        <li>
+                            <a href="#"> 예적금 통합비교</a>
+                        </li>
+                        <li>
+                            <a href="#"> 예금비교</a>
+                        </li>
+                        <li>
+                            <a href="#">적금비교</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </nav>
+        </header>
+    )
+
+};
